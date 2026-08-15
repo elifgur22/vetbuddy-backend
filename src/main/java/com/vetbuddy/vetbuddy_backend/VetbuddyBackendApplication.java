d8e9220a65e6f4984c2f408a6381ff148e12b0cc
Effy22 <elif.gur@bilgeadamboost.com>
2026-08-15 19:59:53 +0300

    Initialize VetBuddy Spring Boot Backend

---
+++ src/main/java/com/vetbuddy/vetbuddy_backend/VetbuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vetbuddy.vetbuddy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetbuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetbuddyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetbuddy/vetbuddy_backend/VetbuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetbuddy.vetbuddy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetbuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
